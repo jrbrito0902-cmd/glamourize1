a11f0f824a2ef99b11920c609c9e70fe11a619c5
Pogo Online <jrbrito.0902@gmail.com>
2026-03-16 21:46:10 -0300

    feat: Add Sanity configuration file to set up the studio with project details and schema types.

--- src/lib/sanity.config.ts
+++ src/lib/sanity.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "../sanity/schema";
 
 export default defineConfig({
   name: "default",
-  title: "Eutimia Modas Admin",
+  title: "E.ESTILO MODAS Admin",
 
   projectId: "ov81cs59",
   dataset: "production",
 
   basePath: "/admin",
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool()],
 
   schema: {
     types: schemaTypes,
