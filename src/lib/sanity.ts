54e0cf2dc0194495aaf30e448f89e801a822e5af
JrBrito <jrbrito.0902@gmail.com>
2026-08-10 15:11:11 -0300

    feat: initialize Sanity client and image URL builder utility

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = createClient({
   projectId: "cw81es59",
   dataset: "production",
-  useCdn: true,
+  useCdn: false,
   apiVersion: "2024-03-01",
 });
 
